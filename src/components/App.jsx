0ff5efd504640b8c59769fcacad96a244626ac25
RomaChumak <romachumak355@gmail.com>
2023-09-28 10:25:37 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,50 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import { Layout } from "./Layout.styled";
 import { Feedbacks } from "./feedbacksList/feedbacks";
 import { StatesticSection } from "./feedbacksStatistics/feedbacksStatistics.styled";
 import { Notification } from "./Notification/Notification";
 import { StatisticList } from "./feedbacksStatistics/feedbacksStatisticsList";
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [feedbacks, setFeedbacks] = useState({
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
+  });
+
+  const { good, neutral, bad } = feedbacks;
+  function hendleClick(feedbacks){
+    setFeedbacks(pFeedbacks => {
+    return { ...pFeedbacks,
+        [feedbacks]: pFeedbacks[feedbacks] + 1,
+      }
+    }
+    )
   };
 
-  hendleClick = (optin) => {
-    this.setState(pState => ({
-        [optin]: pState[optin] +1,
-    }))
+  function countTotalFeedback() {
+    return good + neutral + bad;
   };
 
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good * 100) / this.countTotalFeedback());
+  function countPositiveFeedbackPercentage () {
+    return Math.round((good * 100) / countTotalFeedback());
   }
 
-  render() {
-    const {good, neutral, bad} = this.state
+    
   return (    <Layout>
-    <Feedbacks options={Object.keys(this.state)} onClick={this.hendleClick} />
+    <Feedbacks options={Object.keys(feedbacks)} onClick={hendleClick} />
     <StatesticSection >
        <h2>Statistics</h2>
-          {this.countTotalFeedback() <= 0 ? (
+          {countTotalFeedback() <= 0 ? (
             <Notification message="There is no feedback" />
           ) : (
             <StatisticList
               good={good}
               neutral={neutral}
               bad={bad}
-              total={this.countTotalFeedback()}
-              percentage={this.countPositiveFeedbackPercentage()}
+              total={countTotalFeedback()}
+              percentage={countPositiveFeedbackPercentage()}
             />
           )}
         </StatesticSection>
   </Layout>
   )}
-};
